2673edbd404f881919b9df468dc6d1fce85911b5
Afjal Hossain <abdullahalafjal31@gmail.com>
2026-06-25 16:01:01 +0600

    suppressHydrationWarning added

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col bg-slate-50 text-slate-900">
+      
+      <body suppressHydrationWarning className="min-h-full flex flex-col bg-slate-50 text-slate-900">
         <Navbar />
         <main className="grow">{children}</main>
         <Footer />
